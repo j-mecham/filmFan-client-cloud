7d508bfa881bc8f244f90678e8febb225fefc116
j-mecham <josh.mecham@gmail.com>
2023-07-28 17:30:56 -0600

    Made signup and login forms

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,14 +1,25 @@
 import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    const storedToken = localStorage.getItem("token");
+    const [user, setUser] = useState(storedUser ? storedUser : null);
+    const [token, setToken] = useState(storedToken ? storedToken : null);
     const [movies, setMovies] = useState([]);
-
     const [selectedMovie, setSelectedMovie] = useState(null);
 
     useEffect(() => {
-        fetch("https://filmfanattic-8d1d52c1e608.herokuapp.com/movies")
+        if (!token) {
+            return;
+        }
+
+        fetch("https://filmfanattic-8d1d52c1e608.herokuapp.com/movies", {
+            headers: { Authorization: 'Bearer ${token}' }
+        })
             .then((response) => response.json())
             .then((movies) => {
                 console.log(movies);
@@ -26,7 +37,20 @@ export const MainView = () => {
                 });
                 setMovies(moviesFromApi);
             });
-    }, []);
+    }, [token]);
+
+    if (!user) {
+        return (
+            <>
+                <LoginView onLoggedIn={(user, token) => {
+                    setUser(user);
+                    setToken(token);
+                }} />
+                or
+                <SignupView />
+            </>
+        );
+    }
 
     if (selectedMovie) {
         return (
@@ -40,6 +64,7 @@ export const MainView = () => {
 
     return (
         <div>
+            <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
             {movies.map((movie) => (
                 <MovieCard
                     key={movie.id}
